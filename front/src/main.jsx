4aa1e3f8f74ddb48106deab7af4c07641670ded6
Alexandra Wonyu <alexandrawonyu@gmail.com>
2024-06-17 23:02:26 +0200

    add errorElement

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
 import './index.css';
 import Home from './routes/home.jsx';
 import Root from './routes/root.jsx';
@@ -12,6 +16,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <Navigate to='/' />,
     children: [
       {
         path: '/',
